d4acfb48468fc79257648723f737ef45d56827fb
irisosk90 <irisosk17@ru.is>
2017-12-08 16:21:13 +0000

    Added empty Exception functions

---
+++ include/exceptions/Exceptions.h
@@ -0,0 +1,23 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+
+class Exceptions
+{
+    public:
+        Exceptions();
+        void isNameValid();
+
+        void isSSNValid();
+
+        void isMonthValid();
+
+        void isYearValid();
+
+        void isSalaryValid();
+
+
+    private:
+};
+
+#endif // EXCEPTIONS_H
---
+++ src/exceptions/Exceptions.cpp
@@ -0,0 +1,26 @@
+#include "Exceptions.h"
+
+Exceptions::Exceptions()
+{
+    //ctor
+}
+
+void Exceptions::isNameValid(){
+
+}
+
+void Exceptions::isSSNValid(){
+
+}
+
+void Exceptions::isMonthValid(){
+
+}
+
+void Exceptions::isYearValid(){
+
+}
+
+void Exceptions::isSalaryValid(){
+
+}
