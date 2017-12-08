ac642cc1f82ed897b31b638689093255fbff9b18
irisosk90 <irisosk17@ru.is>
2017-12-08 21:16:33 +0000

    test

--- include/exceptions/Exceptions.h
+++ include/exceptions/Exceptions.h
@@ -1,23 +1,32 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
+#include "Services.h"
+#include <time.h>
+
 
 class Exceptions
 {
     public:
         Exceptions();
-        void isNameValid();
+        bool isNameValid(char employeeName[]);
 
-        void isSSNValid();
+        bool isSSNValid(char SSN[]);
 
-        void isMonthValid();
+        bool isMonthValid(int month);
 
-        void isYearValid();
+        bool isYearValid(int year);
 
-        void isSalaryValid();
+        bool isSalaryValid();
 
 
     private:
-};
+        // taken from stackoverflow.com
+        time_t theTime = time(NULL);
+        struct tm *aTime = localtime(&theTime);
+
+        int currentMonth = aTime->tm_mon + 1; // Month is 0 - 11, add 1 to get a jan-dec 1-12 concept
+        int currentYear = aTime->tm_year + 1900; // Year is # years since 1900
+        };
 
 #endif // EXCEPTIONS_H
